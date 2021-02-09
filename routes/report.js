9ff15e66fd5497aa53d75bce784af9ff02c336fa
unknown <propassionist@gmail.com>
2021-02-10 08:42:14 +0900

    report save 수정 중

--- routes/report.js
+++ routes/report.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   res.render('report');
 });
 
-router.get('/form', function(req, res, next) {
+router.post('/form', function(req, res, next) {
 
   var form = "";
   var mngPoint = "";
@@ -16,9 +16,9 @@ router.get('/form', function(req, res, next) {
     code : req.query.mngPointCd,
     name : req.query.mngPointName
   };
-  if(req.query.rptType == 'A'){
+  if(req.body.rptCd == 'R202011002'){
     form = require("./req_report.json");
-  }else if(req.query.rptType == 'B'){
+  }else if(req.body.rptCd == 'R202011003'){
     form = require("./clean_report.json");
   }
 
@@ -52,17 +52,24 @@ router.post('/save', function(req, res, next) {
         itemcnt = req.body.itemnm.length;
       }
 
-      console.log('itemcnt :: ' + itemcnt);
-      for(var i=0; i<itemcnt; i++){
-
+      var itemnm = req.body.itemnm;
+      var itemtyp = req.body.itemtype;      
+      if(typeof req.body.itemnm === 'string'){
+        itemnm = itemnm.split();
+        itemtyp = itemtyp.split();
+      }      
+      
+      
+      // for(var i=0; i<itemcnt; i++){
+      async.eachOf(itemnm, function(item, key, callback){
         var itemnm = req.body.itemnm;
         var itemtyp = req.body.itemtype;
-        if(Array.isArray(req.body.itemnm)){
-          itemnm = req.body.itemnm[i];
-          itemtyp = req.body.itemtype[i];
-        }
+        // if(Array.isArray(req.body.itemnm)){
+        //   itemnm = req.body.itemnm[i];
+        //   itemtyp = req.body.itemtype[i];
+        // }
 
-        post = {RPTCD : rptCd, BUSSCD : req.session.busscd, ITEMNM : itemnm, ITEMTYP : itemtyp};
+        post = {RPTCD : rptCd, BUSSCD : req.session.busscd, ITEMNM : item, ITEMTYP : itemtyp[key]};
 
         db.query("INSERT INTO SAS_ITEM_MST SET ? ", post
         , function(error, results, fields) {
@@ -71,41 +78,53 @@ router.post('/save', function(req, res, next) {
                 throw error;
             }
 
-            console.log(results.insertId);
-
             var itemCd = results.insertId;
 
             // var item;                        
-            var obj = JSON.parse(JSON.stringify(req.body));
-            var itemKeyArr = obj["itemkey" + (i+1)];
-            var itemValueArr = obj["itemvalue" + (i+1)];
-              
-            for(var j=0; j<itemKeyArr.length; j++){
-              db.query("SELECT COUNT(*) CNT \n" +
-                  "FROM SAS_ITEM_DTL \n" +
-                  "WHERE ITEMCD = ? ", [itemCd]
-              , function(error, results, fields) {
-                  if (error) {
-                      console.log(error);
-                      throw error;
-                  }
+            // var obj = JSON.parse(req.body);
+            var itemKeyArr = req.body["itemkey" + (key+1)];
+            var itemValueArr = req.body["itemvalue" + (key+1)];            
+            
+            // if(itemKeyArr){              
+              async.eachOf(itemKeyArr, function(itemkey, key2, callback){
+                console.log(item);
+              console.log(itemCd);
+              // for(var j=0; j<itemKeyArr.length; j++){
+                db.query("SELECT COUNT(*) CNT \n" +
+                    "FROM SAS_ITEM_DTL \n" +
+                    "WHERE ITEMCD = ? ", [itemCd]
+                , function(error, results, fields) {
+                    if (error) {
+                        console.log(error);
+                        throw error;
+                    }
+                    
+                    console.log(Number(results[0].CNT) + 1);
+                    post = {ITEMCD : itemCd, SEQ : Number(results[0].CNT) + 1, BUSSCD : req.session.busscd, KEY : itemkey, VALUE : itemValueArr[key2]};
 
-                  post = {ITEMCD : itemCd, SEQ : Number(results.CNT) + 1, BUSSCD : req.session.busscd, KEY : itemKeyArr[i], VALUE : itemValueArr[i]};
+                    db.query("INSERT INTO SAS_ITEM_DTL SET ? ", post
+                    , function(error, results, fields) {
+                        if (error) {
+                            console.log(error);
+                            throw error;
+                        }
+                        console.log(db.query)
 
-                  db.query("INSERT INTO SAS_ITEM_DTL SET ? ", post
-                  , function(error, results, fields) {
-                      if (error) {
-                          console.log(error);
-                          throw error;
-                      }
-
-                      res.render("report");
-                  });
+                        // callback(function(){
+                        //   res.render("report");
+                        // });
+                    });
+                });
               });
-            }
+            // }
+            // else{
+            //   callback(function(){
+            //     res.render("report");
+            //   });
+            // }
             
         });
-      }
+      });
 
       // res.render('login', {data: data, msg: req.session.msg,});
 
